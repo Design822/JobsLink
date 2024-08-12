2a5908804dcf60325fb7e8ab288fbd265ba7f62b
VNV Design <vnvdesignandediting@gmail.com>
2024-08-12 16:02:54 +0545

    debugging-files

--- frontend/js/toogle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.querySelector(".toggle-button");
-    const navLinks = document.querySelector("nav.nav-links");
-
-    toggleButton.addEventListener("click", () => {
-        navLinks.classList.toggle("show");
-    });
-});
