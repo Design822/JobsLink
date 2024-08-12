f4042d3446a5a8f161991c3959a090a5f1156ef3
VNV Design <vnvdesignandediting@gmail.com>
2024-08-12 16:02:06 +0545

    divided-header-footer

--- frontend/js/banner.js
+++ frontend/js/banner.js
@@ -1,9 +1,10 @@
-// imgCard = document.querySelectorAll(".img-card");
-// imgCard.forEach(element=>{
-//     element.addEventListener("click", function(e){
-//         imgCard.forEach(el =>{
-//             el.classList.remove("active");
-//         });
-//         element.classList.add("active");
-//     })
-// })
+imgCard = document.querySelectorAll(".img-card");
+imgCard.forEach(element=>{
+    element.addEventListener("click", function(e){
+        imgCard.forEach(el =>{
+            el.classList.remove("active");
+        });
+        element.classList.add("active");
+    })
+})
+
